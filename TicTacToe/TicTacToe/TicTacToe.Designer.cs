3c6538133f7bcc2c9e0cedd29377b84d017209ed
all_caaash <une066@gmail.com>
2025-05-16 18:22:21 +0500

    UI

---
+++ TicTacToe/TicTacToe/TicTacToe.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cell_0_0 = new System.Windows.Forms.Panel();
+            this.cell_0_1 = new System.Windows.Forms.Panel();
+            this.cell_0_2 = new System.Windows.Forms.Panel();
+            this.cell_1_2 = new System.Windows.Forms.Panel();
+            this.cell_1_1 = new System.Windows.Forms.Panel();
+            this.cell_1_0 = new System.Windows.Forms.Panel();
+            this.cell_2_2 = new System.Windows.Forms.Panel();
+            this.cell_2_1 = new System.Windows.Forms.Panel();
+            this.cell_2_0 = new System.Windows.Forms.Panel();
+            this.lblNewGame = new System.Windows.Forms.Label();
+            this.lblPlayer1Name = new System.Windows.Forms.Label();
+            this.lblPlayer2Name = new System.Windows.Forms.Label();
+            this.lblPlayer1Score = new System.Windows.Forms.Label();
+            this.lblPlayer2Score = new System.Windows.Forms.Label();
+            this.lblTurn = new System.Windows.Forms.Label();
+            this.lblTurnPlayerName = new System.Windows.Forms.Label();
+            this.lblReset = new System.Windows.Forms.Label();
+            this.lblStartNewGame = new System.Windows.Forms.Label();
+            this.lblPvC = new System.Windows.Forms.Label();
+            this.lblPvP = new System.Windows.Forms.Label();
+            this.lblExit = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cell_0_0
+            // 
+            this.cell_0_0.BackColor = System.Drawing.Color.White;
+            this.cell_0_0.Location = new System.Drawing.Point(12, 12);
+            this.cell_0_0.Name = "cell_0_0";
+            this.cell_0_0.Size = new System.Drawing.Size(99, 96);
+            this.cell_0_0.TabIndex = 0;
+            // 
+            // cell_0_1
+            // 
+            this.cell_0_1.BackColor = System.Drawing.Color.White;
+            this.cell_0_1.Location = new System.Drawing.Point(117, 12);
+            this.cell_0_1.Name = "cell_0_1";
+            this.cell_0_1.Size = new System.Drawing.Size(99, 96);
+            this.cell_0_1.TabIndex = 1;
+            // 
+            // cell_0_2
+            // 
+            this.cell_0_2.BackColor = System.Drawing.Color.White;
+            this.cell_0_2.Location = new System.Drawing.Point(222, 12);
+            this.cell_0_2.Name = "cell_0_2";
+            this.cell_0_2.Size = new System.Drawing.Size(99, 96);
+            this.cell_0_2.TabIndex = 2;
+            // 
+            // cell_1_2
+            // 
+            this.cell_1_2.BackColor = System.Drawing.Color.White;
+            this.cell_1_2.Location = new System.Drawing.Point(222, 114);
+            this.cell_1_2.Name = "cell_1_2";
+            this.cell_1_2.Size = new System.Drawing.Size(99, 96);
+            this.cell_1_2.TabIndex = 5;
+            // 
+            // cell_1_1
+            // 
+            this.cell_1_1.BackColor = System.Drawing.Color.White;
+            this.cell_1_1.Location = new System.Drawing.Point(117, 114);
+            this.cell_1_1.Name = "cell_1_1";
+            this.cell_1_1.Size = new System.Drawing.Size(99, 96);
+            this.cell_1_1.TabIndex = 4;
+            // 
+            // cell_1_0
+            // 
+            this.cell_1_0.BackColor = System.Drawing.Color.White;
+            this.cell_1_0.Location = new System.Drawing.Point(12, 114);
+            this.cell_1_0.Name = "cell_1_0";
+            this.cell_1_0.Size = new System.Drawing.Size(99, 96);
+            this.cell_1_0.TabIndex = 3;
+            // 
+            // cell_2_2
+            // 
+            this.cell_2_2.BackColor = System.Drawing.Color.White;
+            this.cell_2_2.Location = new System.Drawing.Point(222, 216);
+            this.cell_2_2.Name = "cell_2_2";
+            this.cell_2_2.Size = new System.Drawing.Size(99, 96);
+            this.cell_2_2.TabIndex = 8;
+            // 
+            // cell_2_1
+            // 
+            this.cell_2_1.BackColor = System.Drawing.Color.White;
+            this.cell_2_1.Location = new System.Drawing.Point(117, 216);
+            this.cell_2_1.Name = "cell_2_1";
+            this.cell_2_1.Size = new System.Drawing.Size(99, 96);
+            this.cell_2_1.TabIndex = 7;
+            // 
+            // cell_2_0
+            // 
+            this.cell_2_0.BackColor = System.Drawing.Color.White;
+            this.cell_2_0.Location = new System.Drawing.Point(12, 216);
+            this.cell_2_0.Name = "cell_2_0";
+            this.cell_2_0.Size = new System.Drawing.Size(99, 96);
+            this.cell_2_0.TabIndex = 6;
+            // 
+            // lblNewGame
+            // 
+            this.lblNewGame.Font = new System.Drawing.Font("Linux Biolinum G", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblNewGame.ForeColor = System.Drawing.Color.White;
+            this.lblNewGame.Location = new System.Drawing.Point(332, 55);
+            this.lblNewGame.Name = "lblNewGame";
+            this.lblNewGame.Size = new System.Drawing.Size(241, 53);
+            this.lblNewGame.TabIndex = 9;
+            this.lblNewGame.Text = "New Game";
+            this.lblNewGame.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPlayer1Name
+            // 
+            this.lblPlayer1Name.AutoSize = true;
+            this.lblPlayer1Name.Font = new System.Drawing.Font("Linux Biolinum G", 18F);
+            this.lblPlayer1Name.ForeColor = System.Drawing.Color.White;
+            this.lblPlayer1Name.Location = new System.Drawing.Point(327, 231);
+            this.lblPlayer1Name.Name = "lblPlayer1Name";
+            this.lblPlayer1Name.Size = new System.Drawing.Size(80, 27);
+            this.lblPlayer1Name.TabIndex = 10;
+            this.lblPlayer1Name.Text = "Player:";
+            // 
+            // lblPlayer2Name
+            // 
+            this.lblPlayer2Name.AutoSize = true;
+            this.lblPlayer2Name.Font = new System.Drawing.Font("Linux Biolinum G", 18F);
+            this.lblPlayer2Name.ForeColor = System.Drawing.Color.White;
+            this.lblPlayer2Name.Location = new System.Drawing.Point(327, 258);
+            this.lblPlayer2Name.Name = "lblPlayer2Name";
+            this.lblPlayer2Name.Size = new System.Drawing.Size(118, 27);
+            this.lblPlayer2Name.TabIndex = 11;
+            this.lblPlayer2Name.Text = "Computer:";
+            // 
+            // lblPlayer1Score
+            // 
+            this.lblPlayer1Score.AutoSize = true;
+            this.lblPlayer1Score.Font = new System.Drawing.Font("Linux Biolinum G", 18F);
+            this.lblPlayer1Score.ForeColor = System.Drawing.Color.White;
+            this.lblPlayer1Score.Location = new System.Drawing.Point(532, 231);
+            this.lblPlayer1Score.Name = "lblPlayer1Score";
+            this.lblPlayer1Score.Size = new System.Drawing.Size(23, 27);
+            this.lblPlayer1Score.TabIndex = 12;
+            this.lblPlayer1Score.Text = "0";
+            // 
+            // lblPlayer2Score
+            // 
+            this.lblPlayer2Score.AutoSize = true;
+            this.lblPlayer2Score.Font = new System.Drawing.Font("Linux Biolinum G", 18F);
+            this.lblPlayer2Score.ForeColor = System.Drawing.Color.White;
+            this.lblPlayer2Score.Location = new System.Drawing.Point(532, 258);
+            this.lblPlayer2Score.Name = "lblPlayer2Score";
+            this.lblPlayer2Score.Size = new System.Drawing.Size(23, 27);
+            this.lblPlayer2Score.TabIndex = 13;
+            this.lblPlayer2Score.Text = "0";
+            // 
+            // lblTurn
+            // 
+            this.lblTurn.AutoSize = true;
+            this.lblTurn.Font = new System.Drawing.Font("Linux Biolinum G", 18F);
+            this.lblTurn.ForeColor = System.Drawing.Color.White;
+            this.lblTurn.Location = new System.Drawing.Point(327, 285);
+            this.lblTurn.Name = "lblTurn";
+            this.lblTurn.Size = new System.Drawing.Size(64, 27);
+            this.lblTurn.TabIndex = 14;
+            this.lblTurn.Text = "Turn:";
+            // 
+            // lblTurnPlayerName
+            // 
+            this.lblTurnPlayerName.AutoSize = true;
+            this.lblTurnPlayerName.Font = new System.Drawing.Font("Linux Biolinum G", 18F);
+            this.lblTurnPlayerName.ForeColor = System.Drawing.Color.White;
+            this.lblTurnPlayerName.Location = new System.Drawing.Point(397, 285);
+            this.lblTurnPlayerName.Name = "lblTurnPlayerName";
+            this.lblTurnPlayerName.Size = new System.Drawing.Size(22, 27);
+            this.lblTurnPlayerName.TabIndex = 15;
+            this.lblTurnPlayerName.Text = "?";
+            // 
+            // lblReset
+            // 
+            this.lblReset.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblReset.BackColor = System.Drawing.Color.Gray;
+            this.lblReset.Font = new System.Drawing.Font("Linux Biolinum G", 12F);
+            this.lblReset.ForeColor = System.Drawing.Color.White;
+            this.lblReset.Location = new System.Drawing.Point(332, 12);
+            this.lblReset.Name = "lblReset";
+            this.lblReset.Size = new System.Drawing.Size(75, 40);
+            this.lblReset.TabIndex = 11;
+            this.lblReset.Text = "RESET";
+            this.lblReset.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblStartNewGame
+            // 
+            this.lblStartNewGame.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStartNewGame.BackColor = System.Drawing.Color.Gray;
+            this.lblStartNewGame.Font = new System.Drawing.Font("Linux Biolinum G", 12F);
+            this.lblStartNewGame.ForeColor = System.Drawing.Color.White;
+            this.lblStartNewGame.Location = new System.Drawing.Point(420, 12);
+            this.lblStartNewGame.Name = "lblStartNewGame";
+            this.lblStartNewGame.Size = new System.Drawing.Size(102, 40);
+            this.lblStartNewGame.TabIndex = 11;
+            this.lblStartNewGame.Text = "NEW GAME";
+            this.lblStartNewGame.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPvC
+            // 
+            this.lblPvC.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblPvC.BackColor = System.Drawing.Color.Gray;
+            this.lblPvC.Font = new System.Drawing.Font("Linux Biolinum G", 16F);
+            this.lblPvC.ForeColor = System.Drawing.Color.White;
+            this.lblPvC.Location = new System.Drawing.Point(332, 114);
+            this.lblPvC.Name = "lblPvC";
+            this.lblPvC.Size = new System.Drawing.Size(241, 40);
+            this.lblPvC.TabIndex = 11;
+            this.lblPvC.Text = "Player VS Computer";
+            this.lblPvC.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPvP
+            // 
+            this.lblPvP.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblPvP.BackColor = System.Drawing.Color.Gray;
+            this.lblPvP.Font = new System.Drawing.Font("Linux Biolinum G", 16F);
+            this.lblPvP.ForeColor = System.Drawing.Color.White;
+            this.lblPvP.Location = new System.Drawing.Point(332, 170);
+            this.lblPvP.Name = "lblPvP";
+            this.lblPvP.Size = new System.Drawing.Size(241, 40);
+            this.lblPvP.TabIndex = 11;
+            this.lblPvP.Text = "Player VS Player";
+            this.lblPvP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblExit
+            // 
+            this.lblExit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblExit.BackColor = System.Drawing.Color.Gray;
+            this.lblExit.Font = new System.Drawing.Font("Linux Biolinum G", 12F);
+            this.lblExit.ForeColor = System.Drawing.Color.White;
+            this.lblExit.Location = new System.Drawing.Point(533, 12);
+            this.lblExit.Name = "lblExit";
+            this.lblExit.Size = new System.Drawing.Size(40, 40);
+            this.lblExit.TabIndex = 11;
+            this.lblExit.Text = "X";
+            this.lblExit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblExit.Click += new System.EventHandler(this.lblExit_Click);
+            this.lblExit.MouseEnter += new System.EventHandler(this.lblExit_MouseEnter);
+            this.lblExit.MouseLeave += new System.EventHandler(this.lblExit_MouseLeave);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(585, 327);
+            this.Controls.Add(this.lblPvP);
+            this.Controls.Add(this.lblPvC);
+            this.Controls.Add(this.lblReset);
+            this.Controls.Add(this.lblStartNewGame);
+            this.Controls.Add(this.lblExit);
+            this.Controls.Add(this.lblTurnPlayerName);
+            this.Controls.Add(this.lblTurn);
+            this.Controls.Add(this.lblPlayer2Score);
+            this.Controls.Add(this.lblPlayer1Score);
+            this.Controls.Add(this.lblPlayer2Name);
+            this.Controls.Add(this.lblPlayer1Name);
+            this.Controls.Add(this.lblNewGame);
+            this.Controls.Add(this.cell_2_2);
+            this.Controls.Add(this.cell_2_1);
+            this.Controls.Add(this.cell_1_2);
+            this.Controls.Add(this.cell_2_0);
+            this.Controls.Add(this.cell_1_1);
+            this.Controls.Add(this.cell_0_2);
+            this.Controls.Add(this.cell_1_0);
+            this.Controls.Add(this.cell_0_1);
+            this.Controls.Add(this.cell_0_0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TicTacToe";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel cell_0_0;
+        private System.Windows.Forms.Panel cell_0_1;
+        private System.Windows.Forms.Panel cell_0_2;
+        private System.Windows.Forms.Panel cell_1_2;
+        private System.Windows.Forms.Panel cell_1_1;
+        private System.Windows.Forms.Panel cell_1_0;
+        private System.Windows.Forms.Panel cell_2_2;
+        private System.Windows.Forms.Panel cell_2_1;
+        private System.Windows.Forms.Panel cell_2_0;
+        private System.Windows.Forms.Label lblNewGame;
+        private System.Windows.Forms.Label lblPlayer1Name;
+        private System.Windows.Forms.Label lblPlayer2Name;
+        private System.Windows.Forms.Label lblPlayer1Score;
+        private System.Windows.Forms.Label lblPlayer2Score;
+        private System.Windows.Forms.Label lblTurn;
+        private System.Windows.Forms.Label lblTurnPlayerName;
+        private System.Windows.Forms.Label lblReset;
+        private System.Windows.Forms.Label lblStartNewGame;
+        private System.Windows.Forms.Label lblPvC;
+        private System.Windows.Forms.Label lblPvP;
+        private System.Windows.Forms.Label lblExit;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/TicTacToe.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class TicTacToe : Form
+    {
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private Color _btnDown = Color.FromArgb(64,64,64);
+        private Color _btnUp = System.Drawing.Color.Gray;
+
+        private void ButtonMouseEnter(Label lbl)
+        {
+            lbl.BackColor = _btnDown;
+            Cursor = Cursors.Hand;
+        }
+        private void ButtonMouseLeave(Label lbl)
+        {
+            lbl.BackColor = _btnUp;
+            Cursor = Cursors.Default;
+        }
+
+        private void lblExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void lblExit_MouseEnter(object sender, EventArgs e)
+        {
+            ButtonMouseEnter(lblExit);
+        }
+
+        private void lblExit_MouseLeave(object sender, EventArgs e)
+        {
+            ButtonMouseLeave(lblExit);
+        }
+    }
+}
